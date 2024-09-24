f716c5ac2dc32e41b075ca8b0d12bee2001deaaf
IT24102266 <it24102266@my.sliit.lk>
2024-09-24 21:24:30 +0530

    IT24102266Lab7Q1B

---
+++ Q1/IT24102266Lab7Q1B.java
@@ -0,0 +1,69 @@
+import java.util.Scanner;
+
+public class IT24102266Lab7Q1B
+{
+ public static void main(String args[])
+ { 
+  int marks , total = 0 ,x, st=1;
+  float avg;
+  String grade;
+   
+  Scanner ga = new Scanner (System.in);
+
+  while(st<=3)
+  {
+   System.out.println("Student " + st);
+   x=1;
+   while(x<=4)
+   {
+    System.out.print("Enter Subject marks :");
+    marks = ga.nextInt();
+  
+    total=marks + total;
+    x++;
+   }
+   avg=total/4;
+    if(avg<=100 && avg>=75)
+    grade="Distinction";
+  
+    else if (avg<=74 && avg>=50)
+    grade="Credit";
+  
+    else
+     grade="Fail";
+
+     System.out.println(" Average is : " + avg);
+     System.out.println( " Over all Grade is : " + grade);
+
+
+   st++;
+ }
+ 
+ }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
